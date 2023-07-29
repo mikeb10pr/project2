e444312a8870828e53f4f5084651324ce3430383
MichaelACamacho <MichaelACamacho@IVDL0233>
2023-07-29 13:07:18 -0400

    T1

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -1,15 +1,15 @@
 package day5;
 
 public class DemoDay5 {
-	
-	public static void main(String[] args) { 
-		
-		
-		
-		
-		
+
+	public static void main(String[] args) {
+
+		System.out.println("Sunny!");
+
 	}
 	
-	
+	public void getInfo() {
+		System.out.println("T1");
+	}
 
 }
